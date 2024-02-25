16e65ce493ac1895fbe46e8b9ec5e46e15835c60
Dedarx28 <119298662+Dedarx28@users.noreply.github.com>
2024-02-25 22:58:25 +0530

    Update mainn.js

--- mainn.js
+++ mainn.js
@@ -2,15 +2,15 @@ const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
 
 darkModeToggle.addEventListener('click', () => {
-  // Toggle dark mode class on the body
+
   body.classList.toggle('dark-mode');
 
-  // Save user preference in local storage
+
   const isDarkMode = body.classList.contains('dark-mode');
   localStorage.setItem('darkMode', isDarkMode);
 });
 
-// Check user preference from local storage on page load
+
 const isDarkMode = JSON.parse(localStorage.getItem('darkMode'));
 if (isDarkMode) {
   body.classList.add('dark-mode');
